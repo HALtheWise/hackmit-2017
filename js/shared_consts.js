3295c2102f69f6399f39119564071bb38f3e4285
Eric Miller <eric@legoaces.org>
2017-09-16 17:11:40 -0400

    Added snowfall and melting MVP

--- js/shared_consts.js
+++ js/shared_consts.js
@@ -1,2 +1,9 @@
 grid_width = 1000;
 grid_height = 1000;
+
+snow_altitude = 11.0; // Units
+snow_rate = 1.0; // Units per time
+
+melt_rate = 0.75; // Units of snow per time
+
+water_per_snow = 0.25; // Units of water height equal to one unit of snow height
--- js/simulator.js
+++ js/simulator.js
@@ -2,9 +2,44 @@
 start is an array of arrays of [stone height, snow height, water height]
 returns an array of arrays of [stone height, snow height, water height]
 */
-function simulate_step(start){
+function simulate_step(state){
+	dt = 0.5;
 
-	return start;
+	// Add snow
+	snowfallFunc = function(initial) {
+		altitude = initial[0];
+		snow = initial[1];
+		if (altitude > snow_altitude){
+			snow += snow_rate * dt;
+		}
+		// BEWARE: this may alter the original object in a bad way. Oops.
+		initial[1] = snow;
+		return initial;
+	}
+
+	state = state.map(function(row){return row.map(snowfallFunc)})
+
+	// Melt snow
+	meltingFunc = function(initial){
+		altitude = initial[0];
+		snow = initial[1];
+		water = initial[2];
+
+		melt = melt_rate * dt;
+		melt = Math.min(melt, snow);
+
+		snow -= melt;
+		water += melt * water_per_snow;
+
+		// BEWARE: this may alter the original object in a bad way. Oops.
+		initial[1] = snow;
+		initial[2] = water;
+		return initial;
+	}
+
+	state = state.map(function(row){return row.map(meltingFunc)})
+
+	return state;
 }
 
 function get_sample_state(){
