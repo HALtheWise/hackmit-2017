373f8e74270ad2b489a1bd92ed365b7acad24380
Eric Miller <eric@legoaces.org>
2017-09-16 16:12:25 -0400

    Added shape of shared constants and simulator

---
+++ js/shared_consts.js
@@ -0,0 +1,2 @@
+grid_width = 1000;
+grid_height = 1000;
---
+++ js/simulator.js
@@ -0,0 +1,7 @@
+/*
+start is an array of arrays of stone height
+returns an array of arrays of [stone height, snow height, water height]
+*/
+function simulate(start){
+
+}
