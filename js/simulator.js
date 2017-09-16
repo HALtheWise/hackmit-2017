da49d32fe93b6f4fdabd9b31ffa663aa344d66df
Eric Miller <eric@legoaces.org>
2017-09-16 16:42:05 -0400

    Added sample state

--- js/simulator.js
+++ js/simulator.js
@@ -3,5 +3,14 @@ start is an array of arrays of [stone height, snow height, water height]
 returns an array of arrays of [stone height, snow height, water height]
 */
 function simulate_step(start){
- return start;
+
+	return start;
+}
+
+function get_sample_state(){
+	return [
+		[[15.0,0.0,0.0],[10.0,0.0,0.0],[5.0,0.0,0.0]],
+		[[15.0,0.0,0.0],[10.0,0.0,0.0],[5.0,0.0,0.0]],
+		[[15.0,0.0,0.0],[10.0,0.0,0.0],[5.0,0.0,0.0]]
+		];
 }
